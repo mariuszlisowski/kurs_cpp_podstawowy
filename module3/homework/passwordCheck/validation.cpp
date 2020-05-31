2546588019a06a8e270ae568bf4acc22b521698a
Mariusz Lisowski <mariusz.lisowski@tutanota.com>
2020-05-31 13:41:53 +0200

    PasswordCheck finished & tested

--- module3/homework/passwordCheck/validation.cpp
+++ module3/homework/passwordCheck/validation.cpp
@@ -1,2 +1,76 @@
 #include "validation.hpp"
-// TODO: Put implementations here
+#include <string>
+#include <algorithm>
+#include <cctype>
+//
+std::string getErrorMessage(ErrorCode err) {
+	switch (err) {
+	case ErrorCode::Ok:
+		return "OK";
+		break;
+	case ErrorCode::PasswordsDoesNotMatch:
+		return "Passwords do not match";
+		break;
+	case ErrorCode::PasswordNeedsAtLeastNineCharacters:
+		return "Password needs at least nine characters";
+		break;
+	case ErrorCode::PasswordNeedsAtLeastOneNumber:
+		return "Password needs at least one number";
+		break;
+	case ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter:
+		return "Password needs at least one special character";
+		break;
+	case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter:
+		return "Password needs at least one uppercase letter";
+		break;
+	default:
+		return "";
+	}
+}
+//
+bool isCharSpecial(char ch) {
+	char special[] {'#', '$', '%', '&', '!', '?', '@', '^', '(', ')', '<', '>'};
+	for (const char& c : special) {
+		if (c == ch)
+			return true;
+	}
+	return false;
+}
+//
+bool isNumberInPassword(const std::string& pass) {
+	return std::any_of(pass.begin(), pass.end(), ::isdigit);
+}
+bool isSpecialCharInPassword(const std::string& pass) {
+	return std::any_of(pass.begin(), pass.end(), ::isCharSpecial);
+}
+bool isUppercaseInPassword(const std::string& pass) {
+	return std::any_of(pass.begin(), pass.end(), ::isupper);
+}
+bool doesPasswordsMatch(const std::string& passA, const std::string& passB) {
+	return passA == passB;
+}
+//
+ErrorCode checkPasswordRules(const std::string& pass) {
+	if (pass.size() < 9) {
+		return ErrorCode::PasswordNeedsAtLeastNineCharacters;
+	}
+	else if (!isNumberInPassword(pass)) {
+		return ErrorCode::PasswordNeedsAtLeastOneNumber;	
+	}
+	else if (!isSpecialCharInPassword(pass)) {
+		return ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter;	
+	}
+	else if (!isUppercaseInPassword(pass)) {
+		return ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter;	
+	}
+	else {
+		return ErrorCode::Ok;
+	}
+}
+//
+ErrorCode checkPassword(const std::string& passA, const std::string& passB) {
+	if (doesPasswordsMatch(passA, passB))
+		return checkPasswordRules(passA);
+	else
+		return ErrorCode::PasswordsDoesNotMatch;
+}
--- module3/homework/passwordCheck/validation.hpp
+++ module3/homework/passwordCheck/validation.hpp
@@ -1,2 +1,22 @@
-// TODO: I'm empty :) Put enum and function headers here.
-// Don't forget the header guard
+#pragma once
+
+#include <string>
+
+enum class ErrorCode {
+	Ok,
+	PasswordNeedsAtLeastNineCharacters,
+	PasswordNeedsAtLeastOneNumber,
+	PasswordNeedsAtLeastOneSpecialCharacter,
+	PasswordNeedsAtLeastOneUppercaseLetter,
+	PasswordsDoesNotMatch,
+};
+
+std::string getErrorMessage(ErrorCode);
+
+bool isNumberInPassword(const std::string&);
+bool isSpecialCharInPassword(const std::string&);
+bool isUppercaseInPassword(const std::string&);
+bool doesPasswordsMatch(const std::string&, const std::string&);
+
+ErrorCode checkPasswordRules(const std::string&);
+ErrorCode checkPassword(const std::string&, const std::string&);
