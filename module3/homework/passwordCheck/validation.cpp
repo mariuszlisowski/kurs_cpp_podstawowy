b5c49b85a8de23a0a732462243cc0ebfadeed1ce
Mariusz Lisowski <mariusz.lisowski@tutanota.com>
2020-06-12 16:30:07 +0200

    Code prettified

--- module3/homework/passwordCheck/validation.cpp
+++ module3/homework/passwordCheck/validation.cpp
@@ -2,60 +2,55 @@
 
 #include <algorithm>
 #include <cctype>
-//
+
 std::string getErrorMessage(ErrorCode err) {
-	switch (err) {
-	case ErrorCode::Ok:
-		return "OK";
-	case ErrorCode::PasswordsDoesNotMatch:
-		return "Passwords do not match";
-	case ErrorCode::PasswordNeedsAtLeastNineCharacters:
-		return "Password needs at least nine characters";
-	case ErrorCode::PasswordNeedsAtLeastOneNumber:
-		return "Password needs at least one number";
-	case ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter:
-		return "Password needs at least one special character";
-	case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter:
-		return "Password needs at least one uppercase letter";
-	default:
-		return "";
-	}
+    switch (err) {
+    case ErrorCode::Ok:
+        return "OK";
+    case ErrorCode::PasswordsDoesNotMatch:
+        return "Passwords do not match";
+    case ErrorCode::PasswordNeedsAtLeastNineCharacters:
+        return "Password needs at least nine characters";
+    case ErrorCode::PasswordNeedsAtLeastOneNumber:
+        return "Password needs at least one number";
+    case ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter:
+        return "Password needs at least one special character";
+    case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter:
+        return "Password needs at least one uppercase letter";
+    default:
+        return "";
+    }
 }
-//
+
 bool isNumberInPassword(const std::string& pass) {
-	return std::any_of(pass.begin(), pass.end(), ::isdigit);
+    return std::any_of(pass.begin(), pass.end(), ::isdigit);
 }
 bool isSpecialCharInPassword(const std::string& pass) {
-	return std::any_of(pass.begin(), pass.end(), ::ispunct);
+    return std::any_of(pass.begin(), pass.end(), ::ispunct);
 }
 bool isUppercaseInPassword(const std::string& pass) {
-	return std::any_of(pass.begin(), pass.end(), ::isupper);
+    return std::any_of(pass.begin(), pass.end(), ::isupper);
 }
 bool doesPasswordsMatch(const std::string& passA, const std::string& passB) {
-	return passA == passB;
+    return passA == passB;
 }
-//
+
 ErrorCode checkPasswordRules(const std::string& pass) {
-	if (pass.size() < 9) {
-		return ErrorCode::PasswordNeedsAtLeastNineCharacters;
-	}
-	else if (!isNumberInPassword(pass)) {
-		return ErrorCode::PasswordNeedsAtLeastOneNumber;	
-	}
-	else if (!isSpecialCharInPassword(pass)) {
-		return ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter;	
-	}
-	else if (!isUppercaseInPassword(pass)) {
-		return ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter;	
-	}
-	else {
-		return ErrorCode::Ok;
-	}
+    if (pass.size() < 9) {
+        return ErrorCode::PasswordNeedsAtLeastNineCharacters;
+    }
+    if (!isNumberInPassword(pass)) {
+        return ErrorCode::PasswordNeedsAtLeastOneNumber;
+    }
+    if (!isSpecialCharInPassword(pass)) {
+        return ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter;
+    }
+    if (!isUppercaseInPassword(pass)) {
+        return ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter;
+    }
+    return ErrorCode::Ok;
 }
-//
+
 ErrorCode checkPassword(const std::string& passA, const std::string& passB) {
-	if (doesPasswordsMatch(passA, passB))
-		return checkPasswordRules(passA);
-	else
-		return ErrorCode::PasswordsDoesNotMatch;
+    return (doesPasswordsMatch(passA, passB) ? checkPasswordRules(passA) : ErrorCode::PasswordsDoesNotMatch);
 }
